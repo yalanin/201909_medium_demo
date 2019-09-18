8eee1c818dc9680ee08b8101b115d48057de2437
yalanin <yalanin@gmail.com>
2019-09-18 12:17:43 +0800

    index and edit action for stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,9 +1,9 @@
 class StoriesController < ApplicationController
   before_action :authenticate_member!
+  before_action :find_story, only: [:edit, :update, :destroy]
 
   def new
     @story = current_member.stories.new
-    #debugger
   end
 
   def create
@@ -18,10 +18,29 @@ class StoriesController < ApplicationController
   end
 
   def index
+    @stories = current_member.stories.order(created_at: :desc)
+  end
+
+  def edit
+  end
+
+  def update
+    if @story.update(story_params)
+      redirect_to stories_path, notice: '文章已儲存'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
   end
 
   private
   def story_params
     params.require(:story).permit(:title, :content)
+  end
+
+  def find_story
+    @story = current_member.stories.find(params[:id])
   end
 end
