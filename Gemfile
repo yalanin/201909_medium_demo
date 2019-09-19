427fa3c37f3bf2b8e71e78f198acb011eed1e063
yalanin <yalanin@gmail.com>
2019-09-19 16:32:25 +0800

    add AASM setting to story

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'sass-rails', '~> 5'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 4.0'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.5'
+gem 'aasm', '~> 5.0', '>= 5.0.6'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,8 +1,22 @@
 class Story < ApplicationRecord  
+  include AASM
   default_scope { where(deleted_at: nil) }
 
   belongs_to :member
   validates :title, presence: true
+
+  aasm(column: 'status', no_direct_assignment: true) do
+    state :draft, initial: true
+    state :published
+
+    event :publish do
+      transitions from: :draft, to: :published
+    end
+
+    event :unpublish do
+      transitions from: :published, to: :draft
+    end
+  end
 
   def destroy
     update(deleted_at: Time.now)
