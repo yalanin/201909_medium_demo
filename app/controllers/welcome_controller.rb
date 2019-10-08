416e74162d4d529f18774d769f9acae037cc84f8
yalanin <yalanin@gmail.com>
2019-10-08 17:30:35 +0800

    index page and customized url

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    @stories = Story.all
+    @stories = Story.all.order(created_at: :desc).includes(:member)
+  end
+
+  def show
+  end
+
+  def member
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  # 文章 show 頁面路徑
+  get '@:membername/:story_id', to: 'welcome#show', as: 'story_page'
+  # 作者介紹頁面路徑
+  get '@:membername', to: 'welcome#member', as: 'member_page'
+
   resources :stories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
