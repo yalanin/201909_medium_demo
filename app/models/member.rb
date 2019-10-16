87c00aec830e78166d9439f17d32a52b14b59e94
yalanin <yalanin@gmail.com>
2019-10-16 13:29:18 +0800

    link in member page

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,7 +3,7 @@ class Member < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
+  validates :nickname, presence: true, uniqueness: true
 
   has_many :stories
   has_one_attached :avatar
