9b5912919d56e6a130ecf1c6c25e5f8297a260c4
yalanin <yalanin@gmail.com>
2019-10-17 12:06:42 +0800

    story page link

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -21,6 +21,15 @@ module WelcomeHelper
       sanitize(string)
     end
   end
+
+  def story_avatar_big(member, size: 250)
+    if member.avatar.attached?
+      image_tag member.avatar.variant(resize: "#{size}x#{size}"), class: "member-story-round-image-big"
+    else
+      string = "<img class=\"member-story-round-image-big\">"
+      sanitize(string)
+    end
+  end
   
   def member_page_avatar(member, size: 250)
     if member.avatar.attached?
