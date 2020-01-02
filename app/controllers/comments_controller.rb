ba46f14fae9cc2a27a9d37d07c350850a0fef631
yalanin <yalanin@gmail.com>
2020-01-02 12:24:15 +0800

    comment showing setting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,9 +6,18 @@ class CommentsController < ApplicationController
     @comment = @story.comments.new(comment_params)
     @comment.member = current_member
 
-    if @comment.save
-      render js: "alert('留言已儲存')"
-    else
+    # if @comment.save
+    #   render js: "alert('留言已儲存')"
+    # else
+    #   if comment_params[:content].empty?
+    #     render js: "alert('留言內容不可空白')"
+    #   else
+    #     render js: "alert('系統異常，請稍後再試')"
+    #   end
+    # end
+
+    # 留言改用 JS render
+    unless @comment.save
       if comment_params[:content].empty?
         render js: "alert('留言內容不可空白')"
       else
