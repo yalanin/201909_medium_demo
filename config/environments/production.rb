3c96be1a88847eb1c2fb9e8eca514cd35c894b35
yalanin <yalanin@gmail.com>
2021-01-17 03:05:51 +0800

    bundle update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,11 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+
+  # Please check your Rails app for 'config.i18n.fallbacks = true'.
+  # If you're using I18n (>= 1.1.0) and Rails (< 5.2.2), this should be
+  # 'config.i18n.fallbacks = [I18n.default_locale]'.
+  # If not, fallbacks will be broken in your app by I18n 1.1.x.
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
