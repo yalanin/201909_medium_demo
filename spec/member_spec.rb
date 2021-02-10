9cbb0a4fef7d835efd2d51c131fe90f437b51e1d
yalanin <yalanin@gmail.com>
2021-02-10 13:33:14 +0800

    use let method

--- spec/member_spec.rb
+++ spec/member_spec.rb
@@ -5,12 +5,19 @@ RSpec.describe Member do
   #   @member = Member.create(nickname: 'RSpec 測試', intro: '這是 RSpec 測試創造的 member')
   # end
 
-  def member
-    Member.create(nickname: 'RSpec 測試', intro: '這是 RSpec 測試創造的 member')
-  end
+  # def member
+  #   Member.create(nickname: 'RSpec 測試', intro: '這是 RSpec 測試創造的 member')
+  # end
+
+  let(:member) { Member.create(nickname: 'RSpec 測試', intro: '這是 RSpec 測試創造的 member') }
 
   it 'should create a new member' do
     expect(member.nickname).to eq('RSpec 測試')
+  end
+
+  it 'should assign new value to intro' do
+    member.intro = 'RSpec 測試更新'
+    expect(member.intro).to eq('RSpec 測試更新')
   end
 
   it 'should has intro' do
