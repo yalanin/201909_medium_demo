2861cc95b7d2643162eded11dd6bf1d1a8165b20
yalanin <yalanin@gmail.com>
2021-02-11 11:06:19 +0800

    before and context hook

--- spec/controllers/story_spec.rb
+++ spec/controllers/story_spec.rb
@@ -1,15 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'GET #index' do
+  before(:context) do
+    puts 'this is before context'
+  end
+
+  after(:context) do
+    puts 'this is after context'
+  end
+
+  before(:example) do
+    puts 'this is before example'
+  end
+
+  after(:example) do
+    puts 'this is after example'
+  end
+
   let(:member) { Member.create(nickname: 'RSpec 測試', intro: '這是 RSpec 測試創造的 member') }
   let(:stories) { member.stories }
-  let(:lala) { member.stories.last }
+  let(:new_story) { member.stories.new }
 
   describe 'with story index page' do
     it 'should show all stories of member' do
+      puts 'in the index test'
       Story.create(member_id: member.id, title: 'RSpec 測試')
       member_stories = Story.where(member_id: member.id)
       expect(member_stories).to eq(stories)
+    end
+
+    it 'should show current member nickname in page' do
+      puts 'in the index show member test'
+      expect(member.nickname).to eq ('RSpec 測試')
     end
   end
 end
