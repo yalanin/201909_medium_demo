cb8e152dbee93fa20dc00ec888596bb59305fc99
yalanin <yalanin@gmail.com>
2021-02-11 12:23:26 +0800

    change let variable in nested hook

--- spec/controllers/story_spec.rb
+++ spec/controllers/story_spec.rb
@@ -1,25 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe StoriesController, type: :controller do
-  before(:context) do
-    puts 'this is OUTER before context'
-  end
+  # before(:context) do
+  #   puts 'this is OUTER before context'
+  # end
 
-  after(:context) do
-    puts 'this is OUTER after context'
-  end
+  # after(:context) do
+  #   puts 'this is OUTER after context'
+  # end
 
-  before(:example) do
-    puts 'this is OUTER before example'
-  end
+  # before(:example) do
+  #   puts 'this is OUTER before example'
+  # end
 
-  after(:example) do
-    puts 'this is OUTER after example'
-  end
+  # after(:example) do
+  #   puts 'this is OUTER after example'
+  # end
 
   let(:member) { Member.create(nickname: 'RSpec 測試', intro: '這是 RSpec 測試創造的 member') }
   let(:stories) { member.stories }
-  let(:new_story) { member.stories.new }
+  let(:new_story) { member.stories.new(title: 'RSpec 相同變數測試 level 1') }
+
+  it 'should have top level title name' do
+    expect(new_story.title).to eq('RSpec 相同變數測試 level 1')
+  end
 
   describe 'with story index page' do
     it 'should show all stories of member' do
@@ -36,26 +40,32 @@ RSpec.describe StoriesController, type: :controller do
   end
 
   describe 'with story new page' do
-    before(:context) do
-      puts 'this is INNER before context'
-    end
+    # before(:context) do
+    #   puts 'this is INNER before context'
+    # end
 
-    after(:context) do
-      puts 'this is INNER after context'
-    end
+    # after(:context) do
+    #   puts 'this is INNER after context'
+    # end
 
-    before(:example) do
-      puts 'this is INNER before example'
-    end
+    # before(:example) do
+    #   puts 'this is INNER before example'
+    # end
 
-    after(:example) do
-      puts 'this is INNER after example'
-    end
+    # after(:example) do
+    #   puts 'this is INNER after example'
+    # end
+
+    let(:new_story) { member.stories.new(title: 'RSpec 相同變數測試第二層') }
 
     it 'should build a new story instance' do
-      puts 'in the new story test'
+      # puts 'in the new story test'
       get :new
       expect(new_story).to be_a_new(Story)
+    end
+
+    it 'should change title of story without argument' do
+      expect(new_story.title).to eq('RSpec 相同變數測試第二層')
     end
   end
 end
