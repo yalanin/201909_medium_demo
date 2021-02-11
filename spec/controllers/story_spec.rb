1209f7fd26c04f313077bbb622a2c3ed3adcd9e7
yalanin <yalanin@gmail.com>
2021-02-11 11:45:41 +0800

    nested hook

--- spec/controllers/story_spec.rb
+++ spec/controllers/story_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe 'GET #index' do
+RSpec.describe StoriesController, type: :controller do
   before(:context) do
-    puts 'this is before context'
+    puts 'this is OUTER before context'
   end
 
   after(:context) do
-    puts 'this is after context'
+    puts 'this is OUTER after context'
   end
 
   before(:example) do
-    puts 'this is before example'
+    puts 'this is OUTER before example'
   end
 
   after(:example) do
-    puts 'this is after example'
+    puts 'this is OUTER after example'
   end
 
   let(:member) { Member.create(nickname: 'RSpec 測試', intro: '這是 RSpec 測試創造的 member') }
@@ -32,6 +32,30 @@ RSpec.describe 'GET #index' do
     it 'should show current member nickname in page' do
       puts 'in the index show member test'
       expect(member.nickname).to eq ('RSpec 測試')
+    end
+  end
+
+  describe 'with story new page' do
+    before(:context) do
+      puts 'this is INNER before context'
+    end
+
+    after(:context) do
+      puts 'this is INNER after context'
+    end
+
+    before(:example) do
+      puts 'this is INNER before example'
+    end
+
+    after(:example) do
+      puts 'this is INNER after example'
+    end
+
+    it 'should build a new story instance' do
+      puts 'in the new story test'
+      get :new
+      expect(new_story).to be_a_new(Story)
     end
   end
 end
