0fffddcda9dc31e8470e6bd6f5f13da771ebe84f
yalanin <yalanin@gmail.com>
2021-02-13 15:49:47 +0800

    use described class method

--- spec/models/comments_spec.rb
+++ spec/models/comments_spec.rb
@@ -1,14 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Comment do
-  let(:comment) { Comment.create(content: 'RSpec 測試留言') }
+# RSpec.describe Story do
+  # context 'subject method' do
+    # let(:comment) { Comment.create(content: 'RSpec 測試留言') }
+    # it 'should run with subject method but fail' do
+    #   expect(subject).to eq(Comment.new)
+    # end
 
-  it 'should run with subject method but fail' do
-    expect(subject).to eq(Comment.new)
-  end
+    # it 'should run with subject method' do
+    #   subject.content = 'RSpec 測試留言'
+    #   expect(subject.content).to eq(comment.content)
+    # end
+  # end
 
-  it 'should run with subject method' do
-    subject.content = 'RSpec 測試留言'
-    expect(subject.content).to eq(comment.content)
+  context 'descirbed class method' do
+    subject { described_class.new(content: '主題文字1') }
+    let(:comment) { described_class.new(content: '主題文字2') }
+
+    it 'should have new subject comment' do
+      expect(subject.content).to eq('主題文字1')
+    end
+
+    it 'should have new let comment' do
+      expect(comment.content).to eq('主題文字2')
+    end
   end
 end
