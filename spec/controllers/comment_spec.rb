d640387dadde76b5962430e7bc327c60a9471992
yalanin <yalanin@gmail.com>
2021-02-16 15:17:49 +0800

    raise error matcher

--- spec/controllers/comment_spec.rb
+++ spec/controllers/comment_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controlller do
   describe 'with comment create action' do
-    let(:last_story) { Story.create(clap_counter: 100) }
-    let(:float) { 100.0 }
-    let(:int) { 100 }
+    # let(:last_story) { Story.create(clap_counter: 100) }
+    # let(:float) { 100.0 }
+    # let(:int) { 100 }
 
     # it 'should match with each other' do
     #   expect(last_story.clap_counter).to eq(float)
@@ -20,12 +20,18 @@ RSpec.describe CommentsController, type: :controlller do
     #   expect(last_story.clap_counter).to eql(int)
     # end
 
-    describe 'eq，eql and equal metod' do
-      let(:clap_counter) { [last_story.clap_counter] }
-      let(:same_object) { clap_counter }
-      it 'should equal with order id' do
-        # expect(clap_counter).to equal([100])
-        expect(clap_counter).to equal(same_object)
+    # describe 'eq，eql and equal metod' do
+    #   let(:clap_counter) { [last_story.clap_counter] }
+    #   let(:same_object) { clap_counter }
+    #   it 'should equal with order id' do
+    #     # expect(clap_counter).to equal([100])
+    #     expect(clap_counter).to equal(same_object)
+    #   end
+    # end
+
+    describe 'raise error matcher' do
+      it 'will raise error message' do
+        expect { 3 / 0 }.to raise_error(ZeroDivisionError)
       end
     end
   end
