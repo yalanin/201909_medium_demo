a80301a29985a4d7402dee3003cff8c5813442a8
yalanin <yalanin@gmail.com>
2021-02-17 22:17:20 +0800

    double in rspec

--- spec/controllers/welcome_spec.rb
+++ spec/controllers/welcome_spec.rb
@@ -3,18 +3,37 @@ require 'shared/share_context.rb'
 require 'shared/share_examples.rb'
 
 RSpec.describe WelcomeController, type: :controller do
-  describe 'with welcome show page' do
-    subject { ['<Story id: 4, title: "有圖片"', '<Story id: 5, title: "沒有圖">'] }
-    include_examples 'Active Record result is 2'
-  end
+  # describe 'with welcome show page' do
+  #   subject { ['<Story id: 4, title: "有圖片"', '<Story id: 5, title: "沒有圖">'] }
+  #   include_examples 'Active Record result is 2'
+  # end
 
-  describe 'with welcome show page' do
-    subject { [ '<Comment id: 6, content: "留言測試"', '<Comment id: 7, member_id: 2, story_id: 4, content: "123"' ] }
-    include_examples 'Active Record result is 2'
-  end
+  # describe 'with welcome show page' do
+  #   subject { [ '<Comment id: 6, content: "留言測試"', '<Comment id: 7, member_id: 2, story_id: 4, content: "123"' ] }
+  #   include_examples 'Active Record result is 2'
+  # end
 
-  include_context 'Share Context'
-  it 'can use variable in share context' do
-    expect(@comment.content).to eq('RSpec 共享測試')
+  # include_context 'Share Context'
+  # it 'can use variable in share context' do
+  #   expect(@comment.content).to eq('RSpec 共享測試')
+  # end
+
+  describe 'with welcom show page' do
+    let(:member) { double('double member', say_follow_me: 'follow me!') }
+    let(:allow_member) { double('double member') }
+
+    it 'should do something with double method' do
+      expect(member.say_follow_me).to eq('follow me!')
+    end
+
+    it 'should do something with allow..receive method' do
+      allow(allow_member).to receive(:say_follow_me).and_return('follow me!')
+      expect(allow_member.say_follow_me).to eq('follow me!')
+    end
+
+    it 'should do something with recieve message method' do
+      allow(allow_member).to receive_messages(say_follow_me: 'follow me!')
+      expect(allow_member.say_follow_me).to eq('follow me!')
+    end
   end
 end
